ec90f3f5327e95ae435a9ebac77874ed59c5f39e
Flora Nwakpa <floranwakpa@gmail.com>
2024-06-25 17:28:49 +0100

    added functionality

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 let toggleBtns = document.getElementsByClassName('tab-btn');
 let tabInfo = document.getElementsByClassName('tab-info');
-let workTabs = document.getElementsByClassName('work-tabs')
 
-function showTabInfo() {
-    Array.from(tabInfo).forEach(tab => {
-        if (tab.style.display === 'none') {
-            tab.style.display = 'block';
-            // workTabs.style.background-color = '#B9FF66';
-        } else {
-            tab.style.display = 'none';
-        }
-    });
+function showTabInfo(event) {
+    const tabId = event.currentTarget.getAttribute('data-tab');
+    const targetTab = document.getElementById(`tab-${tabId}`);
+    const workTabsContainer = event.currentTarget.closest('.work-tabs');
+    const plusIcon = event.currentTarget.querySelector('.plus-icon');
+    const minusIcon = event.currentTarget.querySelector('.minus-icon');
+    
+    if (targetTab.style.display === 'none' || targetTab.style.display === '') {
+        targetTab.style.display = 'block';
+        plusIcon.style.display = 'none';
+        minusIcon.style.display = 'block';
+        workTabsContainer.style.backgroundColor = '#B9FF66';
+    } else {
+        targetTab.style.display = 'none';
+        plusIcon.style.display = 'block';
+        minusIcon.style.display = 'none';
+        workTabsContainer.style.backgroundColor = '';
+    }
 }
 
 Array.from(toggleBtns).forEach(toggleBtn => {
